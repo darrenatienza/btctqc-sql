3277be71522d017a4992f072b335d3e1804d28b1
Darren Atienza <darrenatienza@gmail.com>
2021-04-23 16:41:51 +0800

    add changes and views

--- btctqc_changes.sql
+++ btctqc_changes.sql
@@ -1,1 +1,8 @@
+/**04-23-2021*/
+ALTER TABLE user_details ADD gender varchar(100) NULL;
+ALTER TABLE user_details ADD birth_date DATETIME NULL;
+ALTER TABLE surveys ADD start_route varchar(100) NULL;
+ALTER TABLE surveys ADD destination_route varchar(100) NULL;
+
+/** */
 ALTER TABLE btctqc_db.users ADD request_password_reset BOOL DEFAULT 0 NOT NULL;
